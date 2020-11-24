5455b156c1c34f6a8b73da019e8880aaf78ebb66
Alistair Hey <alistair@heyal.co.uk>
2020-11-24 19:40:10 +0000

    Publish multi-arch functions
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- contrib/publish.sh
+++ contrib/publish.sh
@@ -4,23 +4,21 @@ set -e
 # Run this command from one-level higher in the folder path, not this folder.
 
 CLI="faas-cli"
-if ! [ -x "$(command -v faas-cli)" ]; then
-    HERE=`pwd`
-    cd /tmp/
-    curl -sL https://github.com/openfaas/faas-cli/releases/download/0.9.3/faas-cli > faas-cli
-    chmod +x ./faas-cli
-    CLI="/tmp/faas-cli"
+HERE=`pwd`
+cd /tmp/
+curl -sL https://github.com/openfaas/faas-cli/releases/download/0.12.19/faas-cli > faas-cli
+chmod +x ./faas-cli
+CLI="/tmp/faas-cli"
 
-    echo "Returning to $HERE"
-    cd $HERE
-fi
+echo "Returning to $HERE"
+cd $HERE
 
 echo "Working folder: `pwd`"
 
-$CLI up --parallel=4 --skip-deploy
+$CLI publish --parallel=4 --platforms linux/amd64,linux/arm64,linux/arm/v7
 HERE=`pwd`
 cd dashboard
-$CLI up -f stack.yml --skip-deploy
+$CLI publish -f stack.yml --platforms linux/amd64,linux/arm64,linux/arm/v7
 cd $HERE
-$CLI up -f gitlab.yml --skip-deploy
-$CLI up -f aws.yml --skip-deploy
+$CLI publish -f gitlab.yml --platforms linux/amd64,linux/arm64,linux/arm/v7
+$CLI publish -f aws.yml --platforms linux/amd64,linux/arm64,linux/arm/v7
